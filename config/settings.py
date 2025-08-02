044ad4a2329365a23ba730cac1f427fab0644a25
@Hutch-west <joseph.hutch@gmail.com>
2025-08-02 09:47:46 -0700

    Fix: Added Render URL to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-m@lde%55k1547-%u+ar4y0prb$dal$ubz63s*1o6+ubblq&7wt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'squeakycleanvets-website.onrender.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'squeakycleanvets-website.onrender.com']
 
 
 # Application definition
